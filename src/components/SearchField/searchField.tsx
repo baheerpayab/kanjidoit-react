cf0fa8514826b981e7e5846e3d76ef4378d5f736
baheer@baheerpayab.com <121376309+Xienite@users.noreply.github.com>
2026-03-31 00:20:16 +0200

    feat: add TextArea component
    Changes include addition of focus state styling for existing input types.

--- src/components/SearchField/searchField.tsx
+++ src/components/SearchField/searchField.tsx
@@ -2,10 +2,11 @@ import "./searchField.css";
 
 export function SearchField({ onChange }: { onChange: (input: string) => void }) {
   return (
-    <div className="search-input-field input-field">
+    <div className="search-field input-field">
       <input
         className="input-field__input"
         placeholder="Search"
+        type="search"
         onChange={(e) => {
           console.log("pressed");
           onChange(e.currentTarget.value);
